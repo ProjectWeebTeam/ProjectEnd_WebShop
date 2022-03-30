c359b70539a5ae11b98f583d33f9989a41622f6d
PhoebeHa0320 <hoangha2000n@gmail.com>
2022-03-30 21:00:47 +0700

    Template Đăng nhập + admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectEnd_ShopAcc.Models;
+using System.Diagnostics;
+
+namespace ProjectEnd_ShopAcc.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+       
+        }
+         public IActionResult Login2()
+        {
+            return View();
+        }}}
